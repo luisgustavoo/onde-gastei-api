7b8ccde0874f45f5384f7c9d4cfcb64bec22ba22
luisgustavovieirasantos@gmail.com <luisgustavovieirasantos@gmail.com>
2022-01-06 17:46:41 -0300

    Criando testes para método updateExpenseById da classe ExpenseServices close #6

--- test/modules/expenses/services/expenses_services_test.dart
+++ test/modules/expenses/services/expenses_services_test.dart
@@ -3,6 +3,7 @@ import 'package:onde_gastei_api/exceptions/database_exception.dart';
 import 'package:onde_gastei_api/modules/expenses/data/i_expense_repository.dart';
 import 'package:onde_gastei_api/modules/expenses/services/expense_services.dart';
 import 'package:onde_gastei_api/modules/expenses/view_model/expense_save_input_model.dart';
+import 'package:onde_gastei_api/modules/expenses/view_model/expense_update_input_model.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
@@ -44,10 +45,10 @@ void main() {
     test('Should throws DatabaseException', () async {
       //Arrange
       final expenseSaveInputModel = ExpenseSaveInputModel(
-        categoryId: 1,
         description: 'Bla bla',
         date: DateTime.now(),
         value: 1,
+        categoryId: 1,
         userId: 1,
       );
 
@@ -60,6 +61,50 @@ void main() {
       expect(
           () => call(expenseSaveInputModel), throwsA(isA<DatabaseException>()));
       verify(() => repository.createExpense(expenseSaveInputModel)).called(1);
+    });
+  });
+
+  group('Group test ', () {
+    test('Should updateExpenseById with success', () async {
+      //Arrange
+      final expenseUpdateInputModel = ExpenseUpdateInputModel(
+        description: 'Bla bla',
+        value: 1,
+        date: DateTime.now(),
+        categoryId: 1,
+      );
+
+      when(() => repository.updateExpenseById(any(), expenseUpdateInputModel))
+          .thenAnswer((_) async => _);
+
+      //Act
+      await expenseServices.updateExpenseById(1, expenseUpdateInputModel);
+
+      //Assert
+      verify(() => repository.updateExpenseById(any(), expenseUpdateInputModel))
+          .called(1);
+    });
+
+    test('Should updateExpenseById DatabaseException', () async {
+      //Arrange
+      final expenseUpdateInputModel = ExpenseUpdateInputModel(
+        description: 'Bla bla',
+        value: 1,
+        date: DateTime.now(),
+        categoryId: 1,
+      );
+
+      when(() => repository.updateExpenseById(any(), expenseUpdateInputModel))
+          .thenThrow(DatabaseException());
+
+      //Act
+      final call = expenseServices.updateExpenseById;
+
+      //Assert
+      expect(() => call(1, expenseUpdateInputModel),
+          throwsA(isA<DatabaseException>()));
+      verify(() => repository.updateExpenseById(any(), expenseUpdateInputModel))
+          .called(1);
     });
   });
 }
