940c77f4146289531fcfec7c0188e2e7432b9a1c
luisgustavovieirasantos@gmail.com <luisgustavovieirasantos@gmail.com>
2022-01-06 14:40:14 -0300

    Criando testes do método login da classe AuthController

--- test/modules/users/controller/auth_controller_test.dart
+++ test/modules/users/controller/auth_controller_test.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
 
+import 'package:dotenv/dotenv.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:onde_gastei_api/entities/user.dart';
 import 'package:onde_gastei_api/exceptions/user_exists_exception.dart';
 import 'package:onde_gastei_api/logs/i_log.dart';
 import 'package:onde_gastei_api/modules/users/controllers/auth_controller.dart';
 import 'package:onde_gastei_api/modules/users/services/i_user_service.dart';
+import 'package:onde_gastei_api/modules/users/view_model/user_login_input_model.dart';
 import 'package:onde_gastei_api/modules/users/view_model/user_save_input_model.dart';
 import 'package:shelf/shelf.dart';
 import 'package:test/expect.dart';
@@ -17,6 +20,8 @@ import 'mock/mock_ser_service.dart';
 
 class MockUserSaveInputModel extends Mock implements UserSaveInputModel {}
 
+class MockUserLoginInputModel extends Mock implements UserLoginInputModel {}
+
 void main() {
   late ILog log;
   late IUserService service;
@@ -28,7 +33,9 @@ void main() {
     service = MockUserService();
     authController = AuthController(service: service, log: log);
     request = MockShelfRequest();
+    load();
     registerFallbackValue(MockUserSaveInputModel());
+    registerFallbackValue(MockUserLoginInputModel());
   });
 
   group('Group createUser', () {
@@ -36,15 +43,6 @@ void main() {
       //Arrange
       final dataRequestFixture = FixtureReader.getJsonData(
           'modules/users/controller/fixture/create_user_request.json');
-      // final dataRequest =
-      //     jsonDecode(dataRequestFixture) as Map<String, dynamic>;
-
-      // final name = dataRequest['name'].toString();
-      // final email = dataRequest['email'].toString();
-      // final password = dataRequest['password'].toString();
-
-      // final userInputModel =
-      //     UserSaveInputModel(name: name, email: email, password: password);
 
       when(() => request.readAsString())
           .thenAnswer((_) async => dataRequestFixture);
@@ -113,6 +111,60 @@ void main() {
               .contains('Erro ao cadastrar usuário'),
           true);
       verify(() => service.createUser(any())).called(1);
+    });
+  });
+
+  group('Group login', () {
+    test('Should login with succes', () async {
+      //Arrange
+      final dataRequestFixture = FixtureReader.getJsonData(
+          'modules/users/controller/fixture/login_request.json');
+      final userExpected = User(
+        id: 1,
+        name: 'Luis Gustavo',
+        email: 'luisgustavovieirasantos@gmail.com',
+      );
+
+      when(() => request.readAsString())
+          .thenAnswer((_) async => dataRequestFixture);
+
+      when(() => service.login(any())).thenAnswer((_) async => userExpected);
+
+      //Act
+      final response = await authController.login(request);
+
+      //Assert
+      final responseData =
+          jsonDecode(await response.readAsString()) as Map<String, dynamic>;
+      expect(response.statusCode, 200);
+      expect(responseData['access_token'], isNotEmpty);
+      expect(responseData['access_token'].toString().contains('1'), true);
+      verify(() => service.login(any())).called(1);
+    });
+
+    test('Should login throws Exception', () async {
+      //Arrange
+      final dataRequestFixture = FixtureReader.getJsonData(
+          'modules/users/controller/fixture/create_user_request.json');
+
+      when(() => request.readAsString())
+          .thenAnswer((_) async => dataRequestFixture);
+
+      when(() => service.login(any())).thenThrow(Exception());
+
+      //Act
+      final response = await authController.login(request);
+
+      //Assert
+      final responseData =
+          jsonDecode(await response.readAsString()) as Map<String, dynamic>;
+
+      expect(response.statusCode, 500);
+      expect(responseData['message'], isNotEmpty);
+      expect(
+          responseData['message'].toString().contains('Erro ao realizar login'),
+          true);
+      verify(() => service.login(any())).called(1);
     });
   });
 }
