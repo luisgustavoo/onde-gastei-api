46be7bc874cb0427dc82490d57385d7cf6aecf8a
luisgustavovieirasantos@gmail.com <luisgustavovieirasantos@gmail.com>
2022-01-07 11:06:38 -0300

    Criando testes para método updateExpenseById da classe ExpensesController close #8

--- test/modules/expenses/controller/expenses_controller_test.dart
+++ test/modules/expenses/controller/expenses_controller_test.dart
@@ -5,6 +5,7 @@ import 'package:onde_gastei_api/logs/i_log.dart';
 import 'package:onde_gastei_api/modules/expenses/controllers/expenses_controller.dart';
 import 'package:onde_gastei_api/modules/expenses/services/i_expense_services.dart';
 import 'package:onde_gastei_api/modules/expenses/view_model/expense_save_input_model.dart';
+import 'package:onde_gastei_api/modules/expenses/view_model/expense_update_input_model.dart';
 import 'package:shelf/shelf.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
@@ -17,6 +18,9 @@ class MockExpensesServices extends Mock implements IExpenseServices {}
 
 class MockExpenseSaveInputModel extends Mock implements ExpenseSaveInputModel {}
 
+class MockExpenseUpdateInputModel extends Mock
+    implements ExpenseUpdateInputModel {}
+
 void main() {
   late IExpenseServices service;
   late ILog log;
@@ -27,8 +31,9 @@ void main() {
     service = MockExpensesServices();
     log = MockLogger();
     expensesController = ExpensesController(service: service, log: log);
-    registerFallbackValue(MockExpenseSaveInputModel());
     request = MockShelfRequest();
+    registerFallbackValue(MockExpenseSaveInputModel());
+    registerFallbackValue(MockExpenseUpdateInputModel());
   });
 
   group('Group test createExpense', () {
@@ -74,6 +79,56 @@ void main() {
           responseData['message'].toString().contains('Erro ao salvar despesa'),
           true);
       verify(() => service.createExpense(any())).called(1);
+    });
+  });
+
+  group('Group test updateExpenseById', () {
+    test('Should updateExpenseById with success', () async {
+      //Arrange
+      final dataRequestFixture = FixtureReader.getJsonData(
+          'modules/expenses/controller/fixture/update_exepenses_by_id.json');
+
+      when(() => request.readAsString())
+          .thenAnswer((_) async => dataRequestFixture);
+      when(() => service.updateExpenseById(any(), any()))
+          .thenAnswer((_) async => _);
+      //Act
+      final response = await expensesController.updateExpenseById(request, '1');
+
+      //Assert
+      final responseData =
+          jsonDecode(await response.readAsString()) as Map<String, dynamic>;
+      expect(response.statusCode, 200);
+      expect(
+          responseData['message']
+              .toString()
+              .contains('Despesa atualizada com sucesso'),
+          isTrue);
+      verify(() => service.updateExpenseById(any(), any())).called(1);
+    });
+
+    test('Should throws Exception', () async {
+      //Arrange
+      final dataRequestFixture = FixtureReader.getJsonData(
+          'modules/expenses/controller/fixture/update_exepenses_by_id.json');
+
+      when(() => request.readAsString())
+          .thenAnswer((_) async => dataRequestFixture);
+      when(() => service.updateExpenseById(any(), any()))
+          .thenThrow(Exception());
+      //Act
+      final response = await expensesController.updateExpenseById(request, '1');
+
+      //Assert
+      final responseData =
+          jsonDecode(await response.readAsString()) as Map<String, dynamic>;
+      expect(response.statusCode, 500);
+      expect(
+          responseData['message']
+              .toString()
+              .contains('Erro ao atualizar despesa'),
+          isTrue);
+      verify(() => service.updateExpenseById(any(), any())).called(1);
     });
   });
 }
